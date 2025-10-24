ee52147bfe3f42e9cf2a7cbc356762612f628314
Shinia-Gupta <shiniavit@gmail.com>
2025-10-24 22:45:19 +0530

    client component in Next.js

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+
+export default function About() {
+const [name, setName] = useState("User");
+    return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          Dashboard Page
+        </h1>
+        <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          This is the dashboard page of the application.
+        </p>
+      </main>
+    </div>
+  );
+}
