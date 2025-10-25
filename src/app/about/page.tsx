d7eded1ad34a5e844b4773205031f0a322579418
Shinia-Gupta <shiniavit@gmail.com>
2025-10-25 17:52:37 +0530

    Server rendering strategy - 2. Dynamic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,17 @@
 
 // import { useState } from "react";
 
-export default function About() {
+//importing cookies to demonstrate automatic switching to dynamic rendering strategy by nextjs
+import { cookies } from "next/headers";
+
+export default async function About() {
 // const [name, setName] = useState("User"); // This will cause an error in a Server component
-    return (
+
+const cookieStore = await cookies();
+const theme=cookieStore.get("theme")
+console.log("Theme cookie value:", theme?.value);
+
+return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
