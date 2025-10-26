8fae17283897342e201a6ed4f9997b01ad5bac5e
Shinia-Gupta <shiniavit@gmail.com>
2025-10-26 23:05:00 +0530

    interleaving components - 3. Client within a server component

--- src/components/server-component-one.tsx
+++ src/components/server-component-one.tsx
@@ -1,12 +1,14 @@
 import fs from 'fs'
 import { ServerComponentTwo } from './server-component-two';
+import ClientComponentOne from './client-component-one';
 
 export const ServerComponentOne = () => {
     fs.readFileSync('src/components/server-component-one.tsx', 'utf8'); // Server-only API usage
   return (
     <>
   <div>Server Component One</div>
-  <ServerComponentTwo/>
+  {/* <ServerComponentTwo/> */}
+  <ClientComponentOne/>
 </>
 );
 }
