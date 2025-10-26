d239828feadf777fabc9c421a934d88e708758a0
Shinia-Gupta <shiniavit@gmail.com>
2025-10-26 20:56:34 +0530

    Context Providers in Server component pattern

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { useTheme } from "@/src/components/Theme-Provider";
+
 // import { serverSideFunction } from "@/src/utils/server-utils";
 export default function ClientRoutePage() {
-
+const theme=useTheme()
 // const result=serverSideFunction()
     // return <div>Client Route Page - {result}</div>;   
-    return <div>Client Route Page - </div>;   
+    return <div style={{color:theme.colors.primary}}>Client Route Page - </div>;   
 
 }
